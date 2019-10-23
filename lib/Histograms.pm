a2f192dd8ec925b1ec4b3a41cb2bb75d070fe82e
Thomas Fisher-York <tly2@cornell.edu>
2019-10-23 10:40:03 -0400

    remove whitespace at beginning of line before splitting into columns.

--- lib/Histograms.pm
+++ lib/Histograms.pm
@@ -152,6 +152,8 @@ sub load_data_from_file{
          open my $fh_in, "<", $datafile or die "Couldn't open $datafile for reading.\n";
          while (my $line = <$fh_in>) {
             next if($line =~ /^\s*#/); # skip comments
+            $line =~ s/^\s+//;
+         #   $line =~ s/\s+$//;
             my @columns = split(/\s+/, $line);
             #  for my $the_column (@columns_to_histogram) {
             my $data_item = $columns[ $cth - 1 ];
